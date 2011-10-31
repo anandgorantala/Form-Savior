f300e8f15537834ff093f333c74fbe2f1710ed47
ianand2 <ianand2>
2011-10-31 17:30:57 -0400

    added demo pages

---
+++ jquery.formSavior.js
@@ -0,0 +1,40 @@
+(function( $ ){
+  $.fn.formSavior = function(options) {
+	return this.each(function() {
+
+		var cfg = {
+			'msg'   : 'There are unsaved changes on this form'
+      	};
+		
+		if ( options ) { 
+        	$.extend( cfg, options );
+     	}
+		
+		var originals = '';
+		var showalert = true;
+		
+		var $form = $(this);
+		$win = $(window);
+		$doc = $(document);
+		
+		function saveOriginals() {	
+			originals = $form.serialize();
+		}
+		
+		function allowSave() {
+			showalert = false;
+		}
+		
+		function savePrompt() {
+			current = $form.serialize();
+			if(current != originals && showalert === true) {
+				return cfg.msg;
+			}
+		}
+		$doc.ready(saveOriginals);
+		$form.submit(allowSave);
+		$win.bind('beforeunload', savePrompt);
+	});
+
+  };
+})( jQuery );
